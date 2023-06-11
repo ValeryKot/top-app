7ff79ef40a2c99106a830d7b662f1e1996700e53
ValeryKot <kot.valery@gmail.com>
2023-06-11 18:06:50 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const a = 1;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -86,10 +87,10 @@ export default function Home() {
             Deploy <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Instantly deploy your Next.js site to a shareable URL with Vercel. {a}
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
