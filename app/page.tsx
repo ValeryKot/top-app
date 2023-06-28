fa1998d72f65ab68958dfc3ed3642c79f05c8594
ValeryKot <kot.valery@gmail.com>
2023-06-28 09:48:27 +0300

    add button, Htag, P

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Htag } from "@/components";
+import { Htag, Button, P } from "@/components";
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,11 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Текст</Htag>
       <Htag tag='h2'>Текст</Htag>
       <Htag tag='h3'>Текст</Htag>
+      <Button apperance="primary" arrow='right'>Кнопка</Button>
+      <Button apperance="ghost" arrow='right'>Кнопка</Button>
+      <P size='l'>Большой</P>
+      <P size='m'>Средний</P>
+      <P size='s'>Маленький</P>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [
+					  {
+						name: 'preset-default',
+						params: {
+						  overrides: {
+							// disable plugins
+							removeViewBox: false,
+						  },
+						},
+					  },
+					],
+				  },
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
 
-module.exports = nextConfig
+		return config;
+	},
+};
+
+module.exports = nextConfig;
